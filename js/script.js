e4b24229df6641c2ceca3d58e581197f46260bb3
AdrianoItaliano <72036893+AdrianoItaliano@users.noreply.github.com>
2022-11-24 18:16:25 +0100

    JS
    Dodanie funkcji strony danego filmu, mała edycja wyszukiwania, itd

--- js/script.js
+++ js/script.js
@@ -7,75 +7,46 @@
 
 //START STRONY
 //wypisanie popularnych filmow
-const mostPopularMovies = async() => {
-        const popular = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results;
-        let counter = 0;
-        document.getElementById("box1").innerHTML = ("<h1>Most popular movies</h1>")
-        for (const video of popular) {
-            if (counter == 4) {
-                break;
-            }
-            document.getElementById('box1').innerHTML += ("<div class='ext'><img src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
-            counter++;
+const mostPopularMovies = async () => {
+    const popular = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results;
+    let counter = 0;
+    document.getElementById("box1").innerHTML = ("<h1>Most popular movies</h1>")
+    for (const video of popular) {
+        if (counter == 4) {
+            break;
         }
-
+        document.getElementById('box1').innerHTML += ("<div class='ext'><img onclick='getMovie(" + video.id + ")' src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
+        counter++;
     }
-    //test strony filmu
-    //const mostPopularMovies = async () => {
-    //    const popular = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results;
-    //    let counter = 0;
-    //    document.getElementById("box1").innerHTML = ("<h1>Most popular movies</h1>")
-    //    for (const video of popular) {
-    //        if (counter == 5) {
-    //            break;
-    //        }
-    //        const movieId=video.id;
-    //        document.getElementById('box1').innerHTML += ("<div class='ext'><img onclick='getMovie()' src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title +"<br>"+movieId+ "</div>");
-    //        counter++;
-    //    }
-    //
-    //}
-    //const getMovie = async () =>{
-    //    const gh = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results.filter(arr=>arr.id===mostPopularMovies(movieId));    
-    //    let counter = 0;
-    //    for (const video of gh) {
-    //        if (counter == 1) {
-    //            break;
-    //        }
-    //        document.getElementById("box1").innerHTML = ("<h1>"+video.original_title+"</h1>")
-    //        document.getElementById("box1").innerHTML += ("<div class='ext'><img src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
-    //        counter++;
-    //    }
-    //    document.getElementById("box2").remove();
-    //    
-    //}
 
-
+}
+//otwarcie strony danego filmu
+const getMovie = async (id) => {
+    fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US")
+    .then((response) => response.json())
+    .then((data) => {
+    document.getElementById("box1").innerHTML = ("<h1>" + data.original_title + "</h1>")
+    document.getElementById("box1").innerHTML += ("<div class='ext'><img src='https://image.tmdb.org/t/p/w500" + data.backdrop_path + "'><br>" + data.original_title + "</div>");
+    document.getElementById("box2").remove();  
+    })
+  };
 
 //wypisanie po genre
-const genreIsAction = async() => {
-        const genreAction = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results.filter(arr => arr.genre_ids.includes(28));
-        let counter = 0;
-        document.getElementById("box2").innerHTML = ("<h1>Action movies</h1>")
-        for (const video of genreAction) {
-            if (counter == 4) {
-                break;
-            }
-            document.getElementById('box2').innerHTML += ("<div class='ext'><img src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
-            counter++;
+const genreIsAction = async () => {
+    const genreAction = (await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json()).results.filter(arr => arr.genre_ids.includes(28));
+    let counter = 0;
+    document.getElementById("box2").innerHTML = ("<h1>Action movies</h1>")
+    for (const video of genreAction) {
+        if (counter == 4) {
+            break;
         }
-
+        document.getElementById('box2').innerHTML += ("<div class='ext'><img onclick='getMovie(" + video.id + ")' src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
+        counter++;
     }
-    //same zdjecia wypisuje 
-    //(async()=>{
-    //    console.log((await(await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&page=1")).json())
-    //    .results.forEach(element => {
-    //    document.body.innerHTML+=("<img src='https://image.tmdb.org/t/p/w500"+element.backdrop_path+"'>");
-    //    }));
-    //})();
 
+}
 //wypisanie filmow po keyword
-const searchKeyword = async() => {
+const searchKeyword = async () => {
     var word = keyword.value;
     const search = (await (await fetch("https://api.themoviedb.org/3/search/movie?api_key=e794b942eda4421dec0b2efd522974f1&language=en-US&query=" + word + "&page=1&include_adult=false")).json()).results;
     let counter = 0;
@@ -84,7 +55,7 @@ const searchKeyword = async() => {
         if (counter == 4) {
             break;
         }
-        document.getElementById('box1').innerHTML += ("<div class='ext'><img src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
+        document.getElementById('box1').innerHTML += ("<div class='ext'><img onclick='getMovie(" + video.id + ")' src='https://image.tmdb.org/t/p/w500" + video.backdrop_path + "'><br>" + video.original_title + "</div>");
         counter++;
     }
 }
